f27e1c3bb071cf0a1578fbf321ffb1f4834e3b56
Ruiz19746 <ruiz19746@iesmarenostrum.com>
2020-02-17 19:59:13 +0100

    commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#marcador1").click(function() {
-        var x = $("main h2").offset();
+        var x = $("").offset();
         $('html, body').animate({ scrollTop: x.top }, 'slow');
     });
     $("#marcador2").click(function() {
-        var x = $(".Modalidades").offset();
+        var x = $("").offset();
         $('html, body').animate({ scrollTop: x.top }, 'slow');
     });
 });
