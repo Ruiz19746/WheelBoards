a5ec5d88cf0e9f7930f9262807e7bf9ee542ca31
ruiz19746 <ruiz19746@iesmarenostrum.com>
2020-02-28 04:29:44 +0100

    commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-    $("#uno").click(function() {
+    $("#consejos").click(function() {
         var x = $("#consejos").offset();
         $('html, body').animate({ scrollTop: x.top }, 'slow');
     });
-    $("#marcador2").click(function() {
+    $("#modalidades").click(function() {
         var x = $("#modalidades").offset();
         $('html, body').animate({ scrollTop: x.top }, 'slow');
+    });
+    $("#arriba").click(function() {
+        var posicion = $("#arriba").offset();
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+    $("#arriba").css("display", "none");
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 500) {
+            $("#arriba").fadeIn('slow');
+        } else {
+            $("#arriba").fadeOut('slow');
+        }
     });
 });
